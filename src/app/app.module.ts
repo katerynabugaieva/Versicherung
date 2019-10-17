c6f88e45428868599bff4a8960500ca59312c9f3
katerynabugaieva <sonnensmile@gmail.com>
2019-10-17 15:47:30 +0200

    update layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { LayoutComponent } from './layout/layout.component';
 import { FormOfPaymentComponent } from './form-of-payment/form-of-payment.component';
 import { IconComponent } from './icon/icon.component';
 import { KennzeichenartComponent } from './kennzeichenart/kennzeichenart.component';
@@ -15,7 +14,6 @@ import { ZulassungComponent } from './zulassung/zulassung.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent,
     FormOfPaymentComponent,
     IconComponent,
     KennzeichenartComponent,
