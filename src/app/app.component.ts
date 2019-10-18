d3c9e33a2e5db2911540e48024ba44755c62a4ef
katerynabugaieva <sonnensmile@gmail.com>
2019-10-18 11:51:02 +0200

    general form with active data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'versicherung';
+export class AppComponent implements OnInit {
+  public globalString = 'I AM PUBLIC!';
+  public form: FormGroup;
+  constructor() {}
+  ngOnInit() {
+    this.form = new FormGroup({
+      //  financeForm: new FormControl('app component'),
+      finForm: new FormGroup({}),
+      erstzulassungForm: new FormGroup({})
+    });
+    console.log(this.form);
+  }
 }
--- src/app/form-of-payment/form-of-payment.component.ts
+++ src/app/form-of-payment/form-of-payment.component.ts
@@ -1,32 +1,42 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ComponentFactoryResolver } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { AppComponent } from '../app.component';
 
 @Component({
-  selector: "app-form-of-payment",
-  templateUrl: "./form-of-payment.component.html",
-  styleUrls: ["./form-of-payment.component.css"]
+  selector: 'app-form-of-payment',
+  templateUrl: './form-of-payment.component.html',
+  styleUrls: ['./form-of-payment.component.css']
 })
 export class FormOfPaymentComponent implements OnInit {
-  isTooltipVisible: boolean = false;
-  constructor() {}
+  constructor(public finInfoForm: AppComponent) {}
 
-  ngOnInit() {}
+  finInfoData: FormGroup;
+  ngOnInit() {
+    this.finInfoData = new FormGroup({
+      payment: new FormGroup({
+        month: new FormControl('')
+        //  bBarkauf: new FormControl('barkauf'),
+        //  bKredit: new FormControl('kredit'),
+        //  bLeasing: new FormControl('leasinf')
+      })
+    });
+
+    this.finInfoForm.form.controls.finForm = this.finInfoData;
+  }
 
   onFormPaymentClick($event) {
     const clickedElement = $event.target || $event.srcElement;
 
-    if (clickedElement.nodeName === "BUTTON") {
+    if (clickedElement.nodeName === 'BUTTON') {
       const isCertainButtonAlreadyActive = clickedElement.parentElement.querySelector(
-        ".btn-primary"
+        '.btn-primary'
       );
       if (isCertainButtonAlreadyActive) {
-        isCertainButtonAlreadyActive.classList.remove("btn-primary");
+        isCertainButtonAlreadyActive.classList.remove('btn-primary');
       }
 
-      clickedElement.className += " btn-primary";
+      // tslint:disable-next-line:quotemark
+      clickedElement.className += ' btn-primary';
     }
-  }
-
-  tooltipMe() {
-    this.isTooltipVisible = true;
   }
 }
